380bcab418af54aa80fd5d6b382b4a4e79b6957b
Jeffrey Zhong Zeng Jie <s10223330@connect.np.edu.sg>
2021-12-03 00:07:17 +0800

    mostly completed, will touch up and finalize

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+/* Contact Me */
+function myFunction() {
+    alert("The form has been submitted");
+}
+
+/* Heading */
+const burgerMenu = document.getElementById("burgerMenu");
+const forMobileMenu = document.getElementById("forMobileMenu");
+const header = document.getElementById("header");
+
+burgerMenu.addEventListener("click", () => {
+  forMobileMenu.classList.toggle("open");
+  burgerMenu.classList.toggle("close");
+  header.classList.toggle("open");
+})
